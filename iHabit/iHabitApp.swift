7625805bade226c7a11df06d7f6bfbb0286312ff
Philipp Sanktjohanser <venture@mein.gmx>
2022-12-19 06:31:02 +0100

    Initial Commit

---
+++ iHabit/iHabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  iHabitApp.swift
+//  iHabit
+//
+//  Created by Philipp Sanktjohanser on 19.12.22.
+//
+
+import SwiftUI
+
+@main
+struct iHabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
